8331f5e68a23d1c720b4e1ed71eade3498b8c3a0
Mallikarjuna <lankamallikarjuna108@gmail.com>
2025-10-21 13:04:48 +0530

    fixed local server issue

--- portfolio-website/src/main.jsx
+++ portfolio-website/src/main.jsx
@@ -5,9 +5,12 @@ import { ThemeProvider } from './context/ThemeContext'
 import App from './App.jsx'
 import './index.css'
 
+// Use base path for GitHub Pages in production
+const basename = import.meta.env.PROD ? '/portfolio-website' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
