eb463f6ab7f788ea7cec036955b9b1f28424ab78
Mallikarjuna <lankamallikarjuna108@gmail.com>
2025-10-21 17:04:47 +0530

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
 })
